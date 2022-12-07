1b9259d409b7187232aee4c29b44f248e637f962
Maxim <efimov.top.academy@gmail.com>
2022-12-08 02:54:29 +0300

    Добавьте файлы проекта.

---
+++ Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPInterpreterRomeProject
+{
+    internal class Context
+    {
+        public string Input { get; set; }
+        public int Output { get; set; }
+        public Context(string input)
+        {
+            Input = input;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DPInterpreterRomeProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string roman = "MMXVIII";
+            Context context = new Context(roman);
+
+            //Строим 'parse tree'
+            List<Expression> tree = new List<Expression>
+            {
+                new ThousandExpression(),
+                new HundredExpression(),
+                new TenExpression(),
+                new OneExpression()
+            };
+
+            //Интерпритатор
+
+            foreach (Expression exp in tree)
+            {
+                exp.Interpret(context);
+            }
+            Console.WriteLine("{0} = {1}",
+                roman, context.Output);
+        }
+    }
+}
